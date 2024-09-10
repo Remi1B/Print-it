b910b9bab71f356bb0cdcf7bb1da4f7dfddb576e
Remi Baldi <baldi.remi@gmail.com>
2024-09-10 19:17:43 +0200

    Modification slide et dot au clic (étape 4)

--- assets/script.js
+++ assets/script.js
@@ -17,33 +17,64 @@ const slides = [
 	}
 ]
 
-// Sélection des flèches, du conteneur des dots
+// Sélection des flèches, du conteneur des dots et de l'image
 let arrow_left = document.querySelector("#banner .arrow_left")
 let arrow_right = document.querySelector("#banner .arrow_right")
 const dotsContainer = document.querySelector(".dots")
+// Sélection de l'image à modifier
+const bannerImg = document.querySelector(".banner-img")
+// Sélection du tagLine si applicable
+const tagline = document.querySelector("#banner p") 
+
+let currentIndex = 0  // Index de la slide active
+
+
+// Fonction pour mettre à jour l'image et la tagline de la bannière
+function updateSlide(index) {
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}` // Chemin vers la nouvelle image
+    tagline.innerHTML = slides[index].tagLine  // Mettre à jour la tagline si nécessaire
+}
+
+// Fonction pour créer les dots dynamiquement
+function createDots() {
+	slides.forEach((slides, index) => {
+		const dot = document.createElement('div')
+        dot.classList.add('dot')
+        // Ajouter la classe 'dot_selected' au premier dot (pour la première slide)
+        if (index === 0) {
+			dot.classList.add('dot_selected')
+        }
+        dotsContainer.appendChild(dot)
+    })
+}
+
+// Fonction pour mettre à jour les dots actifs
+function updateDots(index) {
+	const allDots = document.querySelectorAll('.dot')
+    allDots.forEach((dot, i) => {
+		if (i === index) {
+			dot.classList.add('dot_selected')
+        } else {
+			dot.classList.remove('dot_selected')
+        }
+    })
+}
 
 // Gestion du clic sur la flèche gauche (précédent)
 arrow_left.addEventListener("click", () => {
 	console.log("Flèche gauche cliquée")
+	currentIndex--
+	updateDots(currentIndex) // Mettre à jour les dots
+	updateSlide(currentIndex)  // Mettre à jour l'image et le texte de la slide
 })
 
 // Gestion du clic sur la flèche droite (suivant)
 arrow_right.addEventListener("click", () => {
 	console.log("Flèche droite cliquée")
+	currentIndex++
+	updateDots(currentIndex) // Mettre à jour les dots
+	updateSlide(currentIndex)  // Mettre à jour l'image et le texte de la slide
 })
-
-// Fonction pour créer les dots dynamiquement
-function createDots() {
-    slides.forEach((slides, index) => {
-        const dot = document.createElement('div')
-        dot.classList.add('dot')
-        // Ajouter la classe 'dot_selected' au premier dot (pour la première slide)
-        if (index === 0) {
-            dot.classList.add('dot_selected')
-        }
-        dotsContainer.appendChild(dot)
-    })
-}
 
 // Appel de la fonction pour créer les dots lors du chargement de la page
 createDots()
